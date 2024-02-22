992d63cd5178de2327b1c6613853e832dfd86685
berkay769 <berkay769@BPC03>
2024-02-22 10:56:45 +0300

    Proje dosyası ekle.

---
+++ isGuvenligi/Controllers/ParametreController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace isGuvenligi.Controllers
+{
+    public class ParametreController : Controller
+    {
+      
+            [Route("Parametre/Cikti/{id}/{ad}")]
+            public string Cikti(int id, string ad)
+            {
+                return $"Parametreden gelen id={id}, ad={ad}";
+            }
+       
+    }
+}
